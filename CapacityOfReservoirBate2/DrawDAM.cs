71ab4eac6cdc94bc1ab7c8fc1d01c09b8c0e7042
huangrui1991 <465338644@qq.com>
2016-03-09 14:52:36 +0800

    Add Dam Layer

--- CapacityOfReservoirBate2/DrawDAM.cs
+++ CapacityOfReservoirBate2/DrawDAM.cs
@@ -10,6 +10,8 @@ using ESRI.ArcGIS.TrackingAnalyst;
 using ESRI.ArcGIS.DataSourcesFile;
 using ESRI.ArcGIS.DataSourcesGDB;
 using ESRI.ArcGIS.esriSystem;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace CapacityOfReservoirBate2
 {
@@ -19,7 +21,8 @@ namespace CapacityOfReservoirBate2
         private INewLineFeedback _LineFeedback = null;
         private bool _IsMouseDown = false;
         private IPoint _CurrentPoint = null;
-        private IPolyline Dam = null;
+        private IPolyline _Dam = null;
+        private int _Count;
 
 
         public VolumeComputerDialog Dialog
@@ -30,7 +33,7 @@ namespace CapacityOfReservoirBate2
 
         public DrawDAM()
         {
-            Dialog = new VolumeComputerDialog();
+            
         }
 
         protected override void OnUpdate()
@@ -40,7 +43,7 @@ namespace CapacityOfReservoirBate2
 
         protected override void OnActivate()
         {
-
+            _Count = 0;
             base.OnActivate();
         }
 
@@ -72,81 +75,100 @@ namespace CapacityOfReservoirBate2
 
         protected override void OnDoubleClick()
         {
-            IPolyline line = null;
-            if (_LineFeedback != null)
-                line = _LineFeedback.Stop();
-
-            if (line != null)
+            try
             {
-                Dam = line;
-                IWorkspaceFactory WorkspaceFactory = new InMemoryWorkspaceFactory();
-                IWorkspaceName workspaceName = WorkspaceFactory.Create(null, "DamWorkspace", null, 0);
-                IName name = (IName)workspaceName;
+                if (_Count > 0)
+                {
+                    ArcMap.Document.ActiveView.Refresh();
+                    return;
+                }
+                    
 
-                // Open the workspace through the name object.
-                IFeatureWorkspace Workspace = (IFeatureWorkspace)name.Open();
-                UID CLSID = new ESRI.ArcGIS.esriSystem.UIDClass();
-                CLSID.Value = "esriGeoDatabase.Feature";
+                IPolyline line = null;
+                if (_LineFeedback != null)
+                    line = _LineFeedback.Stop();
 
-                //IFeatureClass DamFeatureClass = Workspace.CreateFeatureClass("Dam",null,CLSID)
+                for (int i = 0; i < ArcMap.Document.FocusMap.LayerCount; ++i)
+                {
+                    ILayer Layer = ArcMap.Document.FocusMap.get_Layer(i);
+                    if (Layer.Name == "Dam")
+                    {
+                        ArcMap.Document.FocusMap.DeleteLayer(Layer);
+                    }
+                }
+
+
+                if (line != null)
+                {
+                    _Dam = line;
+                    IWorkspaceFactory WorkspaceFactory = new ESRI.ArcGIS.DataSourcesGDB.InMemoryWorkspaceFactoryClass();
+                    IWorkspaceName WorkspaceName = WorkspaceFactory.Create(null, "DamWorkspace", null, 0);
+                    IName Name = (IName)WorkspaceName;
+
+                    IFeatureWorkspace Workspace = (IFeatureWorkspace)Name.Open();
+                    UID CLSID = new ESRI.ArcGIS.esriSystem.UIDClass();
+                    CLSID.Value = "esriGeoDatabase.Feature";
+
+                    IFields Fields = new FieldsClass();
+                    IFieldsEdit FieldsEdit = (IFieldsEdit)Fields;
+
+                    IField OjIDField = new FieldClass();
+                    IFieldEdit OjIDFieldEdit = OjIDField as IFieldEdit;
+                    OjIDFieldEdit.Type_2 = esriFieldType.esriFieldTypeOID;
+                    OjIDFieldEdit.Name_2 = "OBJECTID";
+                    OjIDFieldEdit.AliasName_2 = "OBJECTID";
+                    FieldsEdit.AddField(OjIDField);
+
+                    IField ShapeField = new FieldClass();
+
+                    IFieldEdit ShapeFieldEdit = ShapeField as IFieldEdit;
+                    ShapeFieldEdit.Name_2 = "SHAPE";
+                    ShapeFieldEdit.Type_2 = esriFieldType.esriFieldTypeGeometry;
+                    IGeometryDef GeometryDef = new GeometryDefClass();
+                    IGeometryDefEdit GeometryDefEdit = GeometryDef as IGeometryDefEdit;
+                    GeometryDefEdit.GeometryType_2 = esriGeometryType.esriGeometryPolyline;
+                    GeometryDefEdit.GridCount_2 = 1;
+                    GeometryDefEdit.SpatialReference_2 = ArcMap.Document.FocusMap.SpatialReference;
+                    ShapeFieldEdit.GeometryDef_2 = GeometryDef;
+                    FieldsEdit.AddField(ShapeField);
+
+                    IFieldChecker FieldChecker = new FieldCheckerClass();
+                    IEnumFieldError EnumFieldError = null;
+                    IFields ValidatedFields = null;
+                    FieldChecker.ValidateWorkspace = (IWorkspace)Workspace;
+                    FieldChecker.Validate(Fields, out EnumFieldError, out ValidatedFields);
+
+                    UID EXCLSID = new UIDClass();
+
+                    IFeatureClass DamFeatureClass = Workspace.CreateFeatureClass("Dam", ValidatedFields, CLSID, EXCLSID, esriFeatureType.esriFTSimple, "SHAPE", null);
+                    IFeature Feature = DamFeatureClass.CreateFeature();
+                    Feature.Shape = _Dam as IGeometry;
+                    Feature.Store();
+
+                    ESRI.ArcGIS.Carto.IFeatureLayer FeatureLayer = new ESRI.ArcGIS.Carto.FeatureLayerClass();
+                    FeatureLayer.FeatureClass = DamFeatureClass;
+                    FeatureLayer.Name = DamFeatureClass.AliasName;
+                    FeatureLayer.Visible = true;
+                    ArcMap.Document.ActiveView.FocusMap.AddLayer(FeatureLayer);
+                    //AddTemporalLayer(DamFeatureClass, "EVENTID", "TA_DATE");
+                }
+
+                _LineFeedback = null;
+                _IsMouseDown = false;
+
+                Dialog = new VolumeComputerDialog();
+                Dialog.Show();
+
+                _Count++;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
             }
 
-            _LineFeedback = null;
-            _IsMouseDown = false;
-
-            
-
-            Dialog.Show();
         }
 
-        private void AddTemporalLayer(IFeatureClass featureClass, string eventFieldName, string temporalFieldName)
-        {
-            ITemporalLayer temporalFeatureLayer = new TemporalFeatureLayerClass();
-            IFeatureLayer2 featureLayer = temporalFeatureLayer as IFeatureLayer2;
-            ILayer layer = temporalFeatureLayer as ILayer;
-            ITemporalRenderer temporalRenderer = new CoTrackSymbologyRendererClass();
-            ITemporalRenderer2 temporalRenderer2 = (ITemporalRenderer2)temporalRenderer;
-            IFeatureRenderer featureRenderer = temporalRenderer as IFeatureRenderer;
-            ITrackSymbologyRenderer trackRenderer = temporalRenderer as ITrackSymbologyRenderer;
 
-            if (featureLayer != null)
-            {
-                featureLayer.FeatureClass = featureClass;
-            }
-
-            if (featureRenderer != null)
-            {
-                temporalRenderer.TemporalObjectColumnName = eventFieldName;
-                temporalRenderer.TemporalFieldName = temporalFieldName;
-                temporalFeatureLayer.Renderer = featureRenderer;
-            }
-
-            if (trackRenderer != null)
-            {
-                //Create green color value
-                IRgbColor rgbColor = new RgbColorClass();
-                rgbColor.RGB = 0x00FF00;
-
-                //Create simple thin green line 
-                ISimpleLineSymbol simpleLineSymbol = new SimpleLineSymbolClass();
-                simpleLineSymbol.Color = (IColor)rgbColor;
-                simpleLineSymbol.Width = 1.0;
-
-                //Create simple renderer using line symbol
-                ISimpleRenderer simpleRenderer = new SimpleRendererClass();
-                simpleRenderer.Symbol = (ISymbol)simpleLineSymbol;
-
-                //Apply line renderer as track symbol and enable track rendering
-                trackRenderer.TrackSymbologyRenderer = (IFeatureRenderer)simpleRenderer;
-                trackRenderer.ShowTrackSymbologyLegendGroup = true;
-                temporalRenderer2.TrackRendererEnabled = true;
-            }
-
-            if (layer != null)
-            {
-                ArcMap.Document.FocusMap.AddLayer(layer);
-            }
-        }
     }
 
 }
